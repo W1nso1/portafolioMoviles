0dcc8a3aaf9268ae301b9be4e13c6240114bf2a2
W1nso1 <abdiexl@gmail.com>
2026-03-11 00:50:01 -0700

    u2

---
+++ 21_sumas.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+int factorial(int num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num + factorial(num - 1);
+  }
+}
+
+main() {
+  int num = 0;
+  stdout.writeln('Ingresa el numero de factorial');
+  num = int.parse(stdin.readLineSync()!);
+
+  int resultado = factorial(num);
+
+  stdout.writeln('El factorial de $num es: $resultado');
+}
---
+++ 24_tryCatch.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+int factorial(int num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+void main() {
+  int num = 0;
+
+  stdout.writeln('Ingresa el número para calcular su factorial:');
+
+  try {
+    num = int.parse(stdin.readLineSync()!);
+
+    if (num < 0) {
+      throw FormatException("No se permiten números negativos.");
+    }
+
+    int resultado = factorial(num);
+
+    stdout.writeln('El factorial de $num es: $resultado');
+  } catch (e) {
+    stdout.writeln("Input no válido. $e");
+  }
+}
